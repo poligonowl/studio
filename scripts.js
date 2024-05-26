fe6f8eb31466cf60a67bd43383e69023bd754b05
poligonowl <163364616+poligonowl@users.noreply.github.com>
2024-05-26 22:51:52 +0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,16 +1,1 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const navLinks = document.querySelectorAll('nav a');
 
-    navLinks.forEach(link => {
-        link.addEventListener('click', (event) => {
-            event.preventDefault();
-            const targetId = link.getAttribute('href').substring(1);
-            const targetSection = document.getElementById(targetId);
-
-            window.scrollTo({
-                top: targetSection.offsetTop - 60, // Adjust for fixed header
-                behavior: 'smooth'
-            });
-        });
-    });
-});
