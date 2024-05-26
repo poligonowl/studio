22bcf58843a6dee676952080b48559f3b52bfd4d
poligonowl <163364616+poligonowl@users.noreply.github.com>
2024-05-26 22:42:08 +0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,14 +1,14 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const links = document.querySelectorAll("nav a");
+document.addEventListener('DOMContentLoaded', () => {
+    const navLinks = document.querySelectorAll('nav a');
 
-    links.forEach(link => {
-        link.addEventListener("click", function(event) {
+    navLinks.forEach(link => {
+        link.addEventListener('click', (event) => {
             event.preventDefault();
-            const targetId = this.getAttribute("href").substring(1);
-            const targetElement = document.getElementById(targetId);
+            const targetId = link.getAttribute('href').substring(1);
+            const targetSection = document.getElementById(targetId);
 
             window.scrollTo({
-                top: targetElement.offsetTop,
+                top: targetSection.offsetTop - 60, // Adjust for fixed header
                 behavior: 'smooth'
             });
         });
